c3516de71981a8297c8cd84ebb6f8ac974b90e92
Vivek Thakkar <thakkarv29@gmail.com>
2022-03-20 22:24:52 +0530

    currying

--- currying.js
+++ currying.js
@@ -1,29 +1,35 @@
 /**
+ * WHAT IS CURRYING?
+ * Currying is a function that takes one argument at a time and returns a new function expecting the next argument. It is a transformation of functions that translates a function from callable as f(a, b, c) into callable as f(a)(b)(c).
+ * In other terms, currying is when a function — instead of taking all arguments at one time — takes the first one and returns a new function, which takes the second one and returns a new function, which takes the third one, etc. until all arguments are completed.
+ */
+
+/**
  * Currying using bind method
  */
 
 const multiply = (x, y) => {
-  console.log(x * y);
-};
+  console.log(x * y)
+}
 
-let multipleByTwo = multiply.bind(this, 2);
-multipleByTwo(5); // 10
+const multipleByTwo = multiply.bind(this, 2)
+multipleByTwo(5) // 10
 
-let multipleByThree = multiply.bind(this, 3);
-multipleByThree(5); // 15
+const multipleByThree = multiply.bind(this, 3)
+multipleByThree(5) // 15
 
-let multipleByFour = multiply.bind(this, 4, 10);
-multipleByFour(5); // 40 => 4 * 10 '.' here 5 will act as the 3rd arg
+const multipleByFour = multiply.bind(this, 4, 10)
+multipleByFour(5) // 40 => 4 * 10 '.' here 5 will act as the 3rd arg
 
 /**
  * Currying using function  closures
  */
 
-let multiplyNew = function (x) {
+const multiplyNew = function (x) {
   return function (y) {
-    console.log(x * y);
-  };
-};
+    console.log(x * y)
+  }
+}
 
-let multiplyByTwoNew = multiplyNew(2);
-multiplyByTwoNew(5); // 10 // here inner function forms a closure with the x.
+const multiplyByTwoNew = multiplyNew(2)
+multiplyByTwoNew(5) // 10 // here inner function forms a closure with the x.
