1fdce143517efc947129dc2c2d2356a5f7bca5d9
Vivek Thakkar <thakkarv29@gmail.com>
2022-03-21 23:09:52 +0530

    sum

--- 1_problem.js
+++ 1_problem.js
@@ -1,13 +1,17 @@
 // sum(1)(2)(3)(4)()  => 10
 
-function sum(a) {
-    return function(b) {
-        if(b != undefined) {
-            return sum(a+b);
-        } else {
-            return a;
-        }
+function sum (a) {
+  return function (b) {
+    if (b !== undefined) {
+      return sum(a + b)
+    } else {
+      return a
     }
+  }
 }
 
 console.log(sum(1)(2)(3)(4)())
+
+//
+const sumNew = a => (b) => b ? sum(a + b) : a
+console.log(sumNew(1)(2)(3)(4)())
