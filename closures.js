a92f4bbbb7e630133d4d3eaa2c93fcd85c0243be
Vivek Thakkar <thakkarv29@gmail.com>
2022-05-05 23:22:36 +0530

    closures

--- closures.js
+++ closures.js
@@ -1,3 +1,4 @@
+/**
 function a() {
   var x = 2;
   function b() {
@@ -8,3 +9,44 @@ function a() {
 }
 let y = a();
 y();
+
+ */
+
+
+function x() {
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  }
+}
+// in this case var "i" refers to same "i" always (Ssame memory location)
+x() // 6 6 6 6 6
+
+// ------------------------
+
+function x2() {
+  for (let i = 1; i <= 5; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  }
+}
+// in this case, whenver loop runs, for each and every iteration, "i" is a new variable together (its a new copy of "i" always). since "let" is block scoped and will create a new copy everytime
+// Whenever we do i++, then i=2 is a new copy of variable 
+x2() // 1 2 3 4 5
+
+// -----------------------------
+
+function x() {
+  for (var i = 1; i <= 5; i++) {
+    function close(j) {
+      setTimeout((j) => {
+        console.log(j);
+      }, j * 1000);
+    }
+    close(i)
+  }
+}
+// in this case var "i" refers to same "i" always
+x() // 6 6 6 6 6
